90962ee931c31a7e4c5bb28464b644a767d77e78
pheralb <pablohdez4340@gmail.com>
2024-12-01 13:46:05 +0000

    fix: (playground) update tailwind.config

--- examples/nextjs/src/providers/toastProvider.tsx
+++ examples/nextjs/src/providers/toastProvider.tsx
@@ -1,6 +1,13 @@
 'use client';
 
 import { Toaster, ToastTheme, type ToasterProperties } from '@pheralb/toast';
+import {
+  CircleAlert,
+  CircleCheck,
+  CircleX,
+  InfoIcon,
+  LoaderIcon,
+} from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 const ToastClientProvider = (props: ToasterProperties) => {
@@ -10,6 +17,13 @@ const ToastClientProvider = (props: ToasterProperties) => {
       toastFont="font-sans"
       maxToasts={10}
       theme={theme as ToastTheme}
+      toastIcons={{
+        info: <InfoIcon className="text-blue-500" />,
+        error: <CircleX className="text-red-500" />,
+        warning: <CircleAlert className="text-yellow-500" />,
+        success: <CircleCheck className="text-green-500" />,
+        loading: <LoaderIcon className="animate-spin text-gray-500" />,
+      }}
       {...props}
     />
   );
