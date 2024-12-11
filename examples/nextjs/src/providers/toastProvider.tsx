a39cddf85889d1c8c282b1836eb1e33b75a2c8bd
pheralb <pablohdez4340@gmail.com>
2024-12-11 23:30:19 +0000

    fix (playground): update toast options structure in ToastClientProvider

--- examples/nextjs/src/providers/toastProvider.tsx
+++ examples/nextjs/src/providers/toastProvider.tsx
@@ -14,15 +14,17 @@ const ToastClientProvider = (props: ToasterProperties) => {
   const { theme } = useTheme();
   return (
     <Toaster
-      toastFont="font-sans"
       maxToasts={10}
       theme={theme as ToastTheme}
-      toastIcons={{
-        info: <InfoIcon className="text-blue-500" />,
-        error: <CircleX className="text-red-500" />,
-        warning: <CircleAlert className="text-yellow-500" />,
-        success: <CircleCheck className="text-green-500" />,
-        loading: <LoaderIcon className="animate-spin text-gray-500" />,
+      toastOptions={{
+        font: 'font-sans',
+        icons: {
+          info: <InfoIcon className="text-blue-500" />,
+          error: <CircleX className="text-red-500" />,
+          warning: <CircleAlert className="text-yellow-500" />,
+          success: <CircleCheck className="text-green-500" />,
+          loading: <LoaderIcon className="animate-spin text-gray-500" />,
+        },
       }}
       {...props}
     />
