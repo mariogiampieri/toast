c2c803d3d0200a04de2dedaba0bbf5e9bd114038
pheralb <pablohdez4340@gmail.com>
2024-12-22 14:31:56 +0000

    feat: add toast custom options, extend UI styles

--- docs/src/components/examples/toastCustomOptions.tsx
+++ docs/src/components/examples/toastCustomOptions.tsx
@@ -1,9 +1,11 @@
-import { useDocsStore } from '@/store';
-import { Button } from '@/ui/button';
 import { toast, type ToastOptions } from '@pheralb/toast';
 import { PaletteIcon, RefreshCcw } from 'lucide-react';
-import Codeblock from '../codeblock';
-import { TailwindCSS } from '../icons';
+import { useDocsStore } from '@/store';
+import { cn } from '@/utils';
+
+import { Button } from '@/ui/button';
+import Codeblock from '@/components/codeblock';
+import { TailwindCSS } from '@/components/icons';
 
 const ToastCustomOptions = () => {
   const { applyCustomTheme, setApplyCustomTheme } = useDocsStore();
@@ -11,11 +13,29 @@ const ToastCustomOptions = () => {
   const toastStyles: ToastOptions = {
     headless: true,
     classNames: {
-      toast: '',
-      container: '',
-      actions: '',
-      content: '',
-      icon: '',
+      toast: cn(
+        'font-sans text-sm font-medium',
+        'bg-zinc-200/90 dark:bg-zinc-800/90',
+        'text-zinc-900 dark:text-zinc-100',
+        'border border-zinc-300 dark:border-zinc-700',
+        'rounded-lg shadow-lg',
+        'relative flex items-center space-x-2',
+      ),
+      container: cn('flex items-center space-x-[6px] w-full', 'p-2'),
+      content: cn('flex flex-col space-y-0.5'),
+      actions: {
+        container: cn(''),
+        actionBtn: cn(
+          'px-2 py-1 text-sm font-medium rounded-md',
+          'bg-zinc-300 dark:bg-zinc-700',
+          'text-zinc-900 dark:text-zinc-100',
+          'hover:bg-zinc-400 dark:hover:bg-zinc-600',
+          'focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-zinc-500 dark:focus-visible:ring-zinc-400',
+          'transition duration-200 ease-in-out',
+        ),
+        closeBtn: cn('absolute top-0 right-0'),
+      },
+      icon: cn('flex-shrink-0 h-6 w-6'),
     },
   };
 
@@ -24,6 +44,13 @@ const ToastCustomOptions = () => {
     toast.success({
       text: 'Custom styles added successfully',
       description: '✨ Built with Tailwind CSS',
+      delayDuration: 40000,
+      action: {
+        content: 'Hello',
+        onClick: () => {
+          console.log('Hello, world!');
+        },
+      },
     });
   };
 
@@ -37,6 +64,17 @@ const ToastCustomOptions = () => {
   return (
     <div className="flex flex-col space-y-2 pt-2">
       <div className="flex items-center justify-between">
+        <button
+          onClick={() =>
+            toast.success({
+              text: 'Hello, world!',
+              description: '👋 Welcome',
+              delayDuration: 20000,
+            })
+          }
+        >
+          Show Toast
+        </button>
         <Button
           variant="outline"
           disabled={applyCustomTheme?.headless}
