2c47ffb864c4be1d642faf9852122b73d569b75e
pheralb <pablohdez4340@gmail.com>
2024-12-25 11:42:02 +0000

    chore: add ``prettier-plugin-astro`` to workspace

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,7 +3,15 @@ const config = {
   trailingComma: 'all',
   semi: true,
   singleQuote: true,
-  plugins: ['prettier-plugin-tailwindcss'],
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
 };
 
 export default config;
