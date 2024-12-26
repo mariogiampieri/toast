b971b06209be2d67b267f2a9a2002986075e7129
pheralb <pablohdez4340@gmail.com>
2024-12-26 12:55:16 +0000

    chore: update page extensions in Next.js configuration

--- website/next.config.ts
+++ website/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import { withContentCollections } from "@content-collections/next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
 export default withContentCollections(nextConfig);
