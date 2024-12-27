1a8c51c6dc680f80e7c6931edbc2654a14840040
pheralb <pablohdez4340@gmail.com>
2024-12-27 16:40:53 +0000

    fix: delete unused rehype pretty options

--- website/src/mdx/rehypePrettyOptions.ts
+++
@@ -1,9 +0,0 @@
-import type { Options } from "rehype-pretty-code";
-
-export const rehypePrettyOptions: Options = {
-  keepBackground: false,
-  theme: {
-    dark: "github-dark-dimmed",
-    light: "github-light",
-  },
-};
