52d46b1c66df3c6f378b2d592067741ef7a8f08c
pheralb <pablohdez4340@gmail.com>
2024-12-27 15:44:45 +0000

    feat: add global and playground components to MDX renderer

--- website/src/mdx/components.tsx
+++ website/src/mdx/components.tsx
@@ -1,5 +1,18 @@
 import { useMDXComponent } from "@content-collections/mdx/react";
 
+// Global Components:
+import Hero from "@/components/hero";
+
+// Playground Components:
+import ToastCustomOptions from "@/components/playground/toastCustomOptions";
+import {
+  ToastVariantExamples,
+  ToastActionsExamples,
+  ToastLoadingExample,
+} from "@/components/playground/toast";
+import { Positions, ThemeExamples } from "@/components/playground/toaster";
+import CodeblockMDX from "@/components/codeblock/mdx";
+
 interface MDXComponentsProps {
   code: string;
   className?: string;
@@ -7,5 +20,18 @@ interface MDXComponentsProps {
 
 export function MDX(props: MDXComponentsProps) {
   const Component = useMDXComponent(props.code);
-  return <Component components={{}} />;
+  return (
+    <Component
+      components={{
+        pre: (props) => <CodeblockMDX {...props} />,
+        Hero,
+        Positions,
+        ThemeExamples,
+        ToastCustomOptions,
+        ToastVariantExamples,
+        ToastActionsExamples,
+        ToastLoadingExample,
+      }}
+    />
+  );
 }
