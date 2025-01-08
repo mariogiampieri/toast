5fae190c670bbf7fddbdacc819504dbe3ccf6b8b
pheralb <pablohdez4340@gmail.com>
2025-01-08 23:09:31 +0000

    feat: enhance copy to clipboard button with animation and update hero component to use animated toast button

--- website/src/components/codeblock/copyToClipboard.tsx
+++ website/src/components/codeblock/copyToClipboard.tsx
@@ -1,8 +1,15 @@
 "use client";
 
+import { motion, useAnimation, type Transition } from "motion/react";
 import { copyToClipboard } from "@/utils/copyToClipboard";
 import { toast } from "@pheralb/toast";
-import { CopyIcon } from "lucide-react";
+
+const defaultTransition: Transition = {
+  type: "spring",
+  stiffness: 160,
+  damping: 17,
+  mass: 1,
+};
 
 interface CopyToClipboardBtnProps {
   content: string;
@@ -10,6 +17,8 @@ interface CopyToClipboardBtnProps {
 }
 
 const CopyToClipboardBtn = (props: CopyToClipboardBtnProps) => {
+  const controls = useAnimation();
+
   const handleCopy = async () => {
     try {
       await copyToClipboard(props.content);
@@ -27,10 +36,46 @@ const CopyToClipboardBtn = (props: CopyToClipboardBtnProps) => {
   return (
     <button
       title="Copy to clipboard"
+      onMouseEnter={() => controls.start("animate")}
+      onMouseLeave={() => controls.start("normal")}
       onClick={() => handleCopy()}
       className="text-neutral-500 transition-colors hover:text-black focus:outline-none dark:text-neutral-400 dark:hover:text-white"
     >
-      <CopyIcon size={12} />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="12"
+        height="12"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <motion.rect
+          width="14"
+          height="14"
+          x="8"
+          y="8"
+          rx="2"
+          ry="2"
+          variants={{
+            normal: { translateY: 0, translateX: 0 },
+            animate: { translateY: -3, translateX: -3 },
+          }}
+          animate={controls}
+          transition={defaultTransition}
+        />
+        <motion.path
+          d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"
+          variants={{
+            normal: { x: 0, y: 0 },
+            animate: { x: 3, y: 3 },
+          }}
+          transition={defaultTransition}
+          animate={controls}
+        />
+      </svg>
     </button>
   );
 };
