324ffb2a95a7742aefe88a7eb5176b99e2897ece
pheralb <pablohdez4340@gmail.com>
2025-01-29 12:38:12 +0000

    feat: create customizer + separate playground & customizer components

--- website/src/components/playground/toastAnimation.tsx
+++ website/src/components/playground/toastAnimation.tsx
@@ -1,49 +1,15 @@
 "use client";
 
-import { toast, type ToastAnimations } from "@pheralb/toast";
-
-import { Button } from "@/ui/button";
 import CodeblockClient from "@/components/codeblock/client";
-import { ArrowDownUpIcon, ArrowRightLeftIcon, CheckIcon } from "lucide-react";
 import { useDocsStore } from "@/store";
+import ToastAnimationsCustomizer from "./customizer/toastAnimations";
 
 const ToastAnimation = () => {
-  const { toastAnimation, setToastAnimation } = useDocsStore();
-
-  const handleChangeAnimation = (animation: ToastAnimations) => {
-    setToastAnimation(animation);
-    toast.info({
-      text: `Close me 👀`,
-      description: `✨ ${animation} animation`,
-    });
-  };
+  const { toastAnimation } = useDocsStore();
 
   return (
     <div className="my-2 flex flex-col space-y-2">
-      <div className="flex items-center space-x-2 overflow-y-auto">
-        <Button
-          variant="outline"
-          onClick={() => handleChangeAnimation("slide")}
-        >
-          {toastAnimation === "slide" ? (
-            <CheckIcon size={14} />
-          ) : (
-            <ArrowDownUpIcon size={14} />
-          )}
-          <span>Slide</span>
-        </Button>
-        <Button
-          variant="outline"
-          onClick={() => handleChangeAnimation("swipe")}
-        >
-          {toastAnimation === "swipe" ? (
-            <CheckIcon size={14} />
-          ) : (
-            <ArrowRightLeftIcon size={14} />
-          )}
-          <span>Swipe</span>
-        </Button>
-      </div>
+      <ToastAnimationsCustomizer />
       <CodeblockClient
         code={`<Toaster
   toastOptions={{
