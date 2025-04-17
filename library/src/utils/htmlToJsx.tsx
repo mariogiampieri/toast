48c7639b18b325a03ae59c359b452b9a81ab5f64
mariogiampieri <mariogiampieri@gmail.com>
2025-04-17 14:58:53 -0400

    update function + typing

--- library/src/utils/htmlToJsx.tsx
+++ library/src/utils/htmlToJsx.tsx
@@ -3,8 +3,9 @@ import rehypeParse from "rehype-parse";
 import rehypeReact from "rehype-react";
 import React from "react";
 import sanitizeHtml from "sanitize-html";
+import type { ReactElement } from "react";
 
-export function htmlToJsx(htmlString: string): JSX.Element {
+export function htmlToJsx(htmlString: string): ReactElement {
   const sanitized = sanitizeHtml(htmlString, {
     allowedTags: ["b", "i", "em", "strong", "a", "p", "br", "ul", "li"],
     allowedAttributes: {
@@ -14,10 +15,13 @@ export function htmlToJsx(htmlString: string): JSX.Element {
     },
   });
 
-  return unified()
-    .use(rehypeParse, { fragment: true })
-    .use(rehypeReact, {
-      createElement: React.createElement,
-    })
-    .processSync(sanitized).result as JSX.Element;
+  return (
+    unified()
+      .use(rehypeParse, { fragment: true })
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .use((rehypeReact as any).default, {
+        createElement: React.createElement,
+      })
+      .processSync(sanitized).result as ReactElement
+  );
 }
